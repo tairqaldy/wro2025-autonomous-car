1f92fea9d3bf6d7e447b1fc91aa3875b6f5355da
Tair Kaldybayev <taircaldy.yt@mail.ru>
2025-04-21 11:00:43 +0500

    deleted main_run.py for now to fix conflict

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # main.py
 # Запуск различных режимов работы робота в зависимости от файла mode.txt
 
-from routines.main_run import main_autonomous_run
 from routines.test_drive import run_all_tests
 import sys
 import time
@@ -9,26 +8,22 @@ import time
 def run_selected_mode():
     """
     Читает mode.txt и запускает соответствующий режим:
-    - main  → основной автономный заезд
     - test  → тестовая проверка компонентов
     """
     try:
         with open("mode.txt", "r") as file:
             mode = file.read().strip().lower()
     except FileNotFoundError:
-        print("⚠️ Файл mode.txt не найден. Используем режим 'main'")
-        mode = "main"
+        print("⚠️ Файл mode.txt не найден. Используем режим 'test'")
+        mode = "test"
 
     time.sleep(1)
 
-    if mode == "main":
-        print("🚗 Запуск основного маршрута...")
-        main_autonomous_run()
-    elif mode == "test":
+    if mode == "test":
         print("🔧 Запуск тестового режима...")
         run_all_tests()
     else:
-        print(f"❗️ Неизвестный режим: {mode}. Доступны: 'main', 'test'.")
+        print(f"❗️ Неизвестный режим: {mode}. Доступен только: 'test'")
         sys.exit(1)
 
 if __name__ == "__main__":
